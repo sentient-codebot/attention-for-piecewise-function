a125df337c70ba1f575127e1fe95f4c023673292
sentient-codebot-nan <linnunium@live.com>
2021-10-07 16:23:14 +0200

    preliminaries

---
+++ model.py
@@ -0,0 +1,2 @@
+# model.py
+# define a model here
---
+++ pw_func_gen.py
@@ -0,0 +1,9 @@
+# pw_func_gen.py
+# generate a piece-wise function here 
+
+from torch.utils.data import Dataset
+import torch
+
+class PiecewiseFuncDataset(Dataset):
+    def __init__(self)
+
